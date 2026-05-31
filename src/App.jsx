e159a0e40684b29e59c4c497be443e78a9c99c6e
善福徳行 <zenpuku.life4946.blog@gmail.com>
2026-05-31 10:29:45 +0900

    スマホUI改善

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { satsukiSho2026 } from "./data/satsukiSho2026";
 import { nhkMileCup2026 } from "./data/nhkMileCup2026";
 import { victoriaMile2026 } from "./data/victoriaMile2026";
 import { oaks2026 } from "./data/oaks2026";
-import { nipponDerby2026 } from "./data/nipponDerby2026";
+import { nihonDerby2026 } from "./data/nihonDerby2026";
 function App() {
   const raceCatalog = [
     { year: "2026", course: "阪神", key: "osaka", label: "大阪杯", data: osakaHai2026 },
@@ -16,7 +16,7 @@ function App() {
     { year: "2026", course: "東京", key: "oaks", label: "オークス", data: oaks2026 },
     { year: "2026", course: "東京", key: "nhk", label: "NHKマイルカップ", data: nhkMileCup2026 },
     { year: "2026", course: "東京", key: "victoria", label: "ヴィクトリアマイル", data: victoriaMile2026 },
-    { year: "2026", course: "東京", key: "nippon", label: "日本ダービー", data: nipponDerby2026 },
+    { year: "2026", course: "東京", key: "nippon", label: "日本ダービー", data: nihonDerby2026 },
     { year: "2025", course: "中山", key: "arima", label: "有馬記念", data: arimaKinen2025 },
   ];
 
